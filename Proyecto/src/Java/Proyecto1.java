7544d0f01fc09fa3c7cd5d2ebaec7ff59d233b24
CarlosBouzasClase <carlos.bouzas@educa.madrid.org>
2020-11-11 19:25:07 +0100

    Git tag, volver a primeras versiones

--- Proyecto/src/Java/Proyecto1.java
+++ Proyecto/src/Java/Proyecto1.java
@@ -1,4 +1,9 @@
 package Java;
-
+import java util Scanner;
 public class Proyecto1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+System.out.println("Vamos hacer commit haber que pasa.");
+
+    }
 }
